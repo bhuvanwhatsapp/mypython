eda6c8dbb61b8ad83775d1c6ae82482202e9a8b2
bhuvanwhatsapp <bhuvanballi07@gmail.com>
2025-11-10 16:41:08 +0530

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,14 @@
+a=int(input("enter number 1"))
+b=int(input("enter number2"))
+add=a+b
+sub=a-b
+mult=a*b
+dev=a/b
+dev=a//b
+dev=a%b
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
